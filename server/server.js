5f49d02a0d283835c9bca81cf2253ff159d477df
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2017-03-12 14:34:16 +0530

    DB schema added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path  = require("path");
 var app = express();
 
 app.get("/", function(req, resp){
@@ -7,10 +8,18 @@ app.get("/", function(req, resp){
     });
 });
 
-app.get("/test", function(req, resp){
-    resp.json({
+app.get("/test", function(req, res){
+    res.json({
         "test": "Hey test mnodean..."
     });
+});
+
+app.get("/vendors", function(req, res){
+    res.sendFile(path.join(__dirname + "/sample-jsons/sample-vendor.json"));
+});
+
+app.get("/users", function(req, res){
+    res.sendFile(path.join(__dirname + "/sample-jsons/sample-user.json"));
 });
 
 app.listen(3000);
